608d95f0c15956c27cc3a744246ae0d958aceac2
XiaRongGames <xuxiarong1992@gmail.com>
2025-03-31 15:29:55 -0400

    add bat wander state

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -7,6 +7,8 @@ const EnemyDeathEffect = preload("res://Effects/enemy_death_effect.tscn")
 @export var BAT_KNOCKBACK_FRICTION = 200
 @export var BAT_KNOCKBACK_POWER = 150
 @export var BAT_SOFT_COLLISION_POWER = 400
+@export var BAT_WANDER_BUFFER = 4
+
 enum {
 	IDLE,
 	WANDER,
@@ -23,9 +25,13 @@ var knockback = Vector2.ZERO
 @onready var sprite = $AnimatedSprite
 @onready var hurtbox = $HurtBox
 @onready var softCollision = $SoftCollision
+@onready var wanderController = $WanderController
 
 func _ready() -> void:
+	# randomize bat behavior, instead of seeding
+	randomize()
 	print("bat max health: ", stats.max_health)
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta: float) -> void:
 	knockback = knockback.move_toward(Vector2.ZERO, BAT_KNOCKBACK_FRICTION * delta)
@@ -37,8 +43,19 @@ func _physics_process(delta: float) -> void:
 		IDLE:
 			bat_velocity = bat_velocity.move_toward(Vector2.ZERO, ACCELERATION * delta)
 			seek_player()
+			
+			if wanderController.get_time_left() == 0:
+				start_wander_or_idle()
 		WANDER:
-			pass
+			seek_player()
+			
+			if wanderController.get_time_left() == 0:
+				start_wander_or_idle()
+				
+			accelerate_towards_point(wanderController.target_position, delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= BAT_WANDER_BUFFER:
+				start_wander_or_idle()
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
@@ -59,9 +76,17 @@ func accelerate_towards_point(point, delta):
 	bat_velocity = bat_velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	sprite.flip_h = bat_velocity.x < 0
 	
+func start_wander_or_idle():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(randf_range(1, 3))
+	
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	stats.health -= area.damage
